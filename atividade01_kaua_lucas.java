9afa572db613a6f3c2a649fddf2943a032fb61d7
Drax <Kaualucass1122@gmail.com>
2025-09-11 20:37:04 -0300

    atividade01_kaua_lucas
    11/09/2025

---
+++ atividade01_kaua_lucas.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int c;
+        int d;
+
+        a = 25;
+        b =  a * 3;
+        c = 10;
+        d = a/c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+            
+        
+    }
+}
