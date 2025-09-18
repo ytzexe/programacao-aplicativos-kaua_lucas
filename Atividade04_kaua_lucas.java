d684b81524f0b004e92a16d903e438adc2394ca3
Drax <Kaualucass1122@gmail.com>
2025-09-17 23:11:27 -0300

    Atividade04_kaua_lucas

---
+++ Atividade04_kaua_lucas.java
@@ -0,0 +1,187 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade.pkg04;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner(System.in);
+        // Atividade 01
+        
+//        double num;
+//        
+//        System.out.println("Digite o número desejado: ");
+//            num= entrada.nextDouble();
+//            
+//        if (num < 0) {
+//            System.out.println("Você digitou o número "+num+" e ele é negativo!");
+//        }
+//        else {
+//            System.out.println("Você digitou o número "+num+" e ele é positivo!");
+//      }
+    
+    
+           // Atividade 02
+//    
+//            double num;
+//        
+//        System.out.println("Digite o número desejado: ");
+//            num= entrada.nextDouble();
+//            
+//        if (num < 0) {
+//            System.out.println("Você digitou o número "+num+" e ele é negativo!");
+//        }
+//        if (num==0) {
+//            System.out.println("O numero digitado é Zero!");
+//    }
+//        if (num>0) {
+//            System.out.println("Você digitou o número "+num+" e ele é positivo!");
+//        }
+
+            // Atividade 03
+            
+//            double nota1, nota2, nota3, media;
+//            
+//            System.out.println("Digite a primeira nota: ");
+//                nota1 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a segunda nota: ");
+//                nota2 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a terceira nota: ");
+//                nota3 = entrada.nextDouble();
+//                    
+//            media = (nota1 + nota2 + nota3)/ 3;
+//                System.out.println("Sua média: "+media);
+//            
+//            if (media >= 7) {
+//                System.out.println("Aluno aprovado.");
+//            }
+//            else {
+//                    System.out.println("Aluno reprovado.");
+//                    } 
+//            } 
+
+              // Atividade 04 
+              
+//            double nota1, nota2, nota3, media;
+//            
+//            System.out.println("Digite a primeira nota: ");
+//                nota1 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a segunda nota: ");
+//                nota2 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a terceira nota: ");
+//                nota3 = entrada.nextDouble();
+//                    
+//            media = (nota1 + nota2 + nota3)/ 3;
+//                System.out.println("Sua média: "+media);
+//            
+//            if (media >= 7) {
+//                System.out.println("Aluno aprovado.");
+//            }
+//            if (media<5) {
+//                    System.out.println("Aluno reprovado.");
+//                    } 
+//            if (7 > media && media >= 5) {
+//                    System.out.println("Aluno em recupeção.");
+//                    }
+//            } 
+                
+
+//            Atividade 05
+
+//              int num;
+//               
+//              System.out.println("Digite um numero: ");
+//               num = entrada.nextInt();
+//               
+//              if (num%2==0){
+//                  System.out.println("O número digitado é Par.");
+//              }
+//              
+//              else {
+//                  System.out.println("O número digitado é Impar.");
+//              }
+            
+//             Atividade 06
+
+//               double num1, num2, num3;
+//               
+//               System.out.println("Digite o seu primeiro numero:");
+//               num1 = entrada.nextDouble();
+//               
+//               System.out.println("Digite o seu segundo numero: ");
+//               num2 = entrada.nextDouble();
+//               
+//               System.out.println("Digite o seu terceiro numero: ");
+//               num3 = entrada.nextDouble();
+//               
+//               if (num1 > num2 && num1 > num3){
+//                   System.out.println("Seu maior numero é: "+num1);
+//               }
+//               if (num2 > num1 && num2 > num3){
+//                   System.out.println("Seu maior numero é: "+num2);
+//               }
+//               if (num3 > num1 && num3 > num2){
+//                   System.out.println("Seu maior numero é: "+num3);
+//               }
+
+//              Atividade 07
+
+//                int idade;
+//                
+//                System.out.println("Digite a idade do nadador: ");
+//                idade = entrada.nextInt();
+//                
+//                if (idade < 9) {
+//                    System.out.println("O nadador fará parte da categoria Mirim.");
+//                }
+//                if (idade>=9 && idade<14) {
+//                    System.out.println("O nadador fará parte da categoria Infantil.");
+//                }
+//                if (idade>=14 && idade<18) {
+//                    System.out.println("O nadador fará parte da categoria Juvenil.");
+//                }
+//                if (idade>=18) {
+//                    System.out.println("O nadador fará parte da categoria Adulto.");
+//                }
+
+//               Atividade 08
+
+                 double num1, num2, soma;
+                 
+                System.out.println("Digite o primeiro numero: ");
+                 num1 = entrada.nextDouble();
+                 
+                 System.out.println("Digite o segundo numero: ");
+                 num2 = entrada.nextDouble();
+                 
+                 soma = num1 + num2;
+                 
+                 if (soma > 20) {
+                     System.out.println("A soma dos numero digitados é: "+soma+" e o resultado será: "+(soma+8));
+                 }
+                 
+                 else {
+                     System.out.println("A soma dos numero digitados é: "+soma+" e o resultado será: "+(soma-5));
+                
+            
+            }
+    }
+               
+               
+    
+}
