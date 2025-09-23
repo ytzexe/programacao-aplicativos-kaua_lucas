50cf97a044855c3a16a4567887b4c7d0d3a2d279
ytzexe <kaualucass1122@gmail.com>
2025-09-23 18:14:08 -0300

    :test_tube: Programação De aplicativos

---
+++ Atividade 04.java
@@ -0,0 +1,369 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade04;
+
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade_04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+                Scanner entrada= new Scanner(System.in);
+        // Atividade 01
+        
+//        double num;
+//        
+//        System.out.println("Digite o número desejado: ");
+//            num= entrada.nextDouble();
+//            
+//        if (num < 0) {
+//            System.out.println("Você digitou o número "+num+" e ele é negativo!");
+//        }
+//        else {
+//            System.out.println("Você digitou o número "+num+" e ele é positivo!");
+//      }
+    
+    
+           // Atividade 02
+//    
+//            double num;
+//        
+//        System.out.println("Digite o número desejado: ");
+//            num= entrada.nextDouble();
+//            
+//        if (num < 0) {
+//            System.out.println("Você digitou o número "+num+" e ele é negativo!");
+//        }
+//        if (num==0) {
+//            System.out.println("O numero digitado é Zero!");
+//    }
+//        if (num>0) {
+//            System.out.println("Você digitou o número "+num+" e ele é positivo!");
+//        }
+
+            // Atividade 03
+            
+//            double nota1, nota2, nota3, media;
+//            
+//            System.out.println("Digite a primeira nota: ");
+//                nota1 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a segunda nota: ");
+//                nota2 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a terceira nota: ");
+//                nota3 = entrada.nextDouble();
+//                    
+//            media = (nota1 + nota2 + nota3)/ 3;
+//                System.out.println("Sua média: "+media);
+//            
+//            if (media >= 7) {
+//                System.out.println("Aluno aprovado.");
+//            }
+//            else {
+//                    System.out.println("Aluno reprovado.");
+//                    } 
+//            } 
+
+              // Atividade 04 
+              
+//            double nota1, nota2, nota3, media;
+//            
+//            System.out.println("Digite a primeira nota: ");
+//                nota1 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a segunda nota: ");
+//                nota2 = entrada.nextDouble();
+//                
+//             System.out.println("Digite a terceira nota: ");
+//                nota3 = entrada.nextDouble();
+//                    
+//            media = (nota1 + nota2 + nota3)/ 3;
+//                System.out.println("Sua média: "+media);
+//            
+//            if (media >= 7) {
+//                System.out.println("Aluno aprovado.");
+//            }
+//            if (media<5) {
+//                    System.out.println("Aluno reprovado.");
+//                    } 
+//            if (7 > media && media >= 5) {
+//                    System.out.println("Aluno em recupeção.");
+//                    }
+//            } 
+                
+
+//            Atividade 05
+
+//              int num;
+//               
+//              System.out.println("Digite um numero: ");
+//               num = entrada.nextInt();
+//               
+//              if (num%2==0){
+//                  System.out.println("O número digitado é Par.");
+//              }
+//              
+//              else {
+//                  System.out.println("O número digitado é Impar.");
+//              }
+            
+//             Atividade 06
+
+//               double num1, num2, num3;
+//               
+//               System.out.println("Digite o seu primeiro numero:");
+//               num1 = entrada.nextDouble();
+//               
+//               System.out.println("Digite o seu segundo numero: ");
+//               num2 = entrada.nextDouble();
+//               
+//               System.out.println("Digite o seu terceiro numero: ");
+//               num3 = entrada.nextDouble();
+//               
+//               if (num1 > num2 && num1 > num3){
+//                   System.out.println("Seu maior numero é: "+num1);
+//               }
+//               if (num2 > num1 && num2 > num3){
+//                   System.out.println("Seu maior numero é: "+num2);
+//               }
+//               if (num3 > num1 && num3 > num2){
+//                   System.out.println("Seu maior numero é: "+num3);
+//               }
+
+//              Atividade 07
+
+//                int idade;
+//                
+//                System.out.println("Digite a idade do nadador: ");
+//                idade = entrada.nextInt();
+//                
+//                if (idade < 9) {
+//                    System.out.println("O nadador fará parte da categoria Mirim.");
+//                }
+//                if (idade>=9 && idade<14) {
+//                    System.out.println("O nadador fará parte da categoria Infantil.");
+//                }
+//                if (idade>=14 && idade<18) {
+//                    System.out.println("O nadador fará parte da categoria Juvenil.");
+//                }
+//                if (idade>=18) {
+//                    System.out.println("O nadador fará parte da categoria Adulto.");
+//                }
+
+//               Atividade 08
+
+//                 double num1, num2, soma;
+//                 
+//                System.out.println("Digite o primeiro numero: ");
+//                 num1 = entrada.nextDouble();
+//                 
+//                 System.out.println("Digite o segundo numero: ");
+//                 num2 = entrada.nextDouble();
+//                 
+//                 soma = num1 + num2;
+//                 
+//                 if (soma > 20) {
+//                     System.out.println("A soma dos numero digitados é: "+soma+" e o resultado será: "+(soma+8));
+//                 }
+//                 
+//                 else {
+//                     System.out.println("A soma dos numero digitados é: "+soma+" e o resultado será: "+(soma-5));
+
+//                Atividade 09
+
+//                  double venda, lucro;
+//                  
+//                  System.out.println("Digite o preço do produto: ");
+//                  venda = entrada.nextDouble();
+//                  
+//                  lucro=venda/100;
+//                  
+//                  if (venda < 20) {
+//                      System.out.println("O valor de venda será: R$"+((lucro*45)+venda));
+//                  }
+//                  else {
+//                       System.out.println("O valor de venda será: R$"+((lucro*30)+venda));
+//                              }
+//               
+//                  Atvidade 10
+//
+//                    double compra, maca;
+//                    int quantidade;
+//                    
+//                    System.out.println("Digite a quantidade de maçãs: ");
+//                    quantidade= entrada.nextInt();
+//                    
+//                    if (quantidade < 12) {
+//                        maca=0.25;
+//                        compra=quantidade*maca;
+//                        System.out.println("O valor total da compra de "+quantidade+" maçãs, será de: R$"+compra);
+//                    }
+//                    else {
+//                        maca=0.30;
+//                        compra=quantidade*maca;
+//                        System.out.println("O valor total da compra de "+quantidade+" maçãs, será de: R$"+compra);
+//                    }
+//                    
+//                     Atvidade 11
+                    
+//                       int sexo;
+//                       double altura, peso;
+//                       
+//                       System.out.println("Digite seu sexo: \nDigite 1 para Feminino. \nDigite 2 para Masculino.");
+//                       sexo = entrada.nextInt();
+//                       
+//                       
+//                       switch (sexo) {
+//                           case 1:
+//                                System.out.println("Digite sua altura: ");
+//                                altura=entrada.nextDouble();
+//                                peso=(65.1*altura)-44.7;
+//                                System.out.println("Seu peso ideal é de: "+peso+" Kg.");
+//                                break;
+//                                
+//                           case 2:
+//                                 System.out.println("Digite sua altura: ");
+//                                 altura=entrada.nextDouble();
+//                                 peso=(72.7*altura)-58;
+//                                 System.out.println("Seu peso ideal é de: "+peso+" Kg.");
+//                                 break;
+//                           default:
+//                                 System.out.println("Digite apenas 1 para feminino ou 2 para mascolino.");
+                                 
+                          
+//                       Atividade 12
+
+//                         int num;
+//                         
+//                            System.out.println("Digite o dia da semana usando somente numeros de 1 a 7: ");
+//                                num = entrada.nextInt();
+//                         
+//                            switch (num) {
+//                                case 1:
+//                                    System.out.println("Domingo é final de semana");
+//                                    break;
+//                                    
+//                                case 2:
+//                                    System.out.println("Segunda-Feira, dia util.");
+//                                    break;
+//                                    
+//                                case 3:
+//                                    System.out.println("Terça-Feira, dia util.");
+//                                    break;
+//                                case 4:
+//                                    System.out.println("Quarta-Feira, dia util.");
+//                                    break;
+//                                case 5:
+//                                    System.out.println("Quinta-Feira, dia util.");
+//                                    break;
+//                                case 6:
+//                                    System.out.println("Sexta-feira, dia util.");
+//                                    break;
+//                                case 7:
+//                                    System.out.println("Sabado é final de semana.");
+//                                    break;
+//                                default:
+//                                    System.out.println("Número digitado invalido");
+//                        }
+
+//                          Atvidade 13
+//
+//                            int codigo, quantidade;
+//                            double preco, total;
+//                            total=0;
+//                            System.out.println("Produtos disponiveis em estoque: \nDetergente -> R$1,59 \nEsponja -> R$5,59 \nLã de aço -> R$1,79");
+//                            System.out.println("Digite o código do produto desejado: \nDetergente -> Cod. 100 \nEsponja -> cod. 101 \nLã de Aço -> Cod. 102 \nOu digite 4 para exibir o total.");
+//                            codigo= entrada.nextInt();
+//                            switch (codigo) {
+//                                case 100:
+//                                    System.out.println("Você selecionou o Detergente! \nDigite a quantidade desejada: ");
+//                                    quantidade= entrada.nextInt();
+//                                    total=total+(1.59*quantidade);
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                case 101:
+//                                    System.out.println("Você selecionou a Esponja! \nDigite a quantidade desejada: ");
+//                                    quantidade= entrada.nextInt();
+//                                    total=total+(4.59*quantidade);
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                case 102:
+//                                    System.out.println("Você selecionou a Lã de Aço! \nDigite a quantidade desejada: ");
+//                                    quantidade= entrada.nextInt();
+//                                    total=total+(1.79*quantidade);
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                case 4:
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                default:
+//                                    System.out.println("Numero invalido!");
+//                                      
+//                            }
+
+
+//                             Atividade 14
+
+//                int operacao;
+//                double num1, num2, total;
+//                System.out.println("Vamos calcular. \nEscolha a operação: \n1 - SOMA \n2 - SUBTRAÇÃO \n3 - MULTIPLICAÇÃO \n4 - DIVISÃO");
+//                operacao= entrada.nextInt();
+//                
+//                switch (operacao) {
+//                    case 1:
+//                        System.out.println("Você escolheu a Soma! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        total=num1+num2;
+//                        System.out.println("O total será: "+total);
+//                        break;
+//                    case 2:
+//                        System.out.println("Você escolheu a Subtração! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        total=num1-num2;
+//                        System.out.println("O total será: "+total);
+//                        break;
+//                    case 3:
+//                        System.out.println("Você escolheu a Multiplicação! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        total=num1*num2;
+//                        System.out.println("O total será: "+total);
+//                        break;
+//                    case 4:
+//                        System.out.println("Você escolheu a Divisão! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        if (num2!=0) {
+//                        total=num1/num2;
+//                        System.out.println("O total será: "+total); 
+//                        break; }
+//                        if (num2==0) {
+//                            System.out.println("O Divisor não pode ser Zero!");
+//                        break; }
+//                    default :
+//                        System.out.println("Opção invalida!");
+//                }
+//                 }
+//          }
+          
+          
+          
+          
+
---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# Programação De Aplicativos - Kauã Lucas
+=======
+# programacao-aplicativos-kaua_lucas
+>>>>>>> e831c40 (:test_tube: Programação De Aplicativos)
+ Repositório da Disciplina de Programação de Aplicativos
