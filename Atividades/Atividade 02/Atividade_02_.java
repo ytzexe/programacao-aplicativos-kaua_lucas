7e610bcf46967a36e5e0463d802dbcec58c550d8
Drax <Kaualucass1122@gmail.com>
2025-10-06 20:47:05 -0300

    Add files via upload

---
+++ Atividades/Atividade 02/Atividade_02_.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade_02_;
+
+/**
+ *
+ * @author Aluno
+ */
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Atividade_02_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+
+        // ================== Atividade 01 ==================
+        /*
+        double raio, circ, area;
+
+        System.out.print("Digite o valor do raio do círculo: ");
+        raio = entrada.nextDouble();
+
+        area = Math.PI * (Math.pow(raio, 2));
+        circ = 2 * Math.PI * raio;
+
+        System.out.println("Área: " + area);
+        System.out.println("Circunferência: " + circ);
+        */
+
+        // ================== Atividade 02 ==================
+        /*
+        double imc, peso, altura;
+        DecimalFormat df = new DecimalFormat("0.0");
+
+        System.out.print("Digite seu peso em KG: ");
+        peso = entrada.nextDouble();
+
+        System.out.print("Digite sua altura em Metros: ");
+        altura = entrada.nextDouble();
+
+        imc = peso / (altura * altura);
+        System.out.println("Seu IMC será: " + df.format(imc));
+        */
+
+        // ================== Atividade 03 ==================
+        /*
+        double cat1, cat2, hipo, raiz;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.print("Digite o valor do primeiro cateto: ");
+        cat1 = entrada.nextDouble();
+
+        System.out.print("Digite o valor do segundo cateto: ");
+        cat2 = entrada.nextDouble();
+
+        hipo = (cat1 * cat1) + (cat2 * cat2);
+        raiz = Math.sqrt(hipo);
+
+        System.out.println("Sua Hipotenusa será: " + df.format(raiz));
+        */
+
+        // ================== Atividade 04 ==================
+        /*
+        double real, dolar, cotacao, troco;
+        int val_cot;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.print("Digite o valor em R$ a ser convertido: ");
+        real = entrada.nextDouble();
+
+        dolar = 5.32;
+
+        cotacao = (real / dolar);
+        val_cot = (int) Math.round(cotacao);
+
+        System.out.print("Com R$" + real);
+        System.out.println(" você consegue: US$" + val_cot);
+
+        troco = real - (val_cot * dolar);
+        System.out.println("Troco: R$" + df.format(troco));
+        */
+
+        // ================== Atividade 05 ==================
+        /*
+        int escolha, dado;
+        
+        System.out.print("Digite a quantidade de rodadas: ");
+        escolha = entrada.nextInt();
+
+        Random rodada = new Random();
+        for (int i = 0; i < escolha; i++) {
+            dado = rodada.nextInt(6) + 1;
+            System.out.println("Rodada " + (i + 1) + ": " + dado);
+        }
+        */
+
+        entrada.close();
+    }
+    
+}
