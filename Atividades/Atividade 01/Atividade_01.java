5da1be23040123fca47d0a454e6e60d2095b9836
Drax <Kaualucass1122@gmail.com>
2025-10-06 20:46:45 -0300

    Add files via upload

---
+++ Atividades/Atividade 01/Atividade_01.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade_01;
+
+/**
+ *
+ * @author Aluno
+ */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Atividade_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // The main method must be in a class named "Main".
+
+        int a;
+        int b;
+        int c;
+        int d;
+
+        a = 25;
+        b =  a * 3;
+        c = 10;
+        d = a/c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+    
+}
