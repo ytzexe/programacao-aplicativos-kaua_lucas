211437e97772616ab903599c09a041f0e8073d75
Kauã Lucas <Kaualucass1122@gmail.com>
2025-10-14 21:33:24 -0300

    Create RegistroVeiculo.java

---
+++ Projeto Prático I/RegistroVeiculo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto.pratico;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ResistroVeiculo (String[] args)  {
+    
+    do{
+        System.out.print("--- Tipo de Veículo ---\n"
+                + " 1- Carro\n"
+                + " 2- Moto\n"
+                + " 3- Caminhão/Caminhonete\n"
+                + " Digite o tipo do veículo: ");
+                escolhaVeiculo = input.nextInt();
+                input.nextInt();
+        
+        switch(Registro){
+            
+            case -> 1
+                    
+            System.out.print("Digite a placa do veiculo: ")
+            String placa = input.nextLine();
+            
+                if (placa.length() == 7){
+            
+        } else {
+                System.out.println("Placa inválida! Deve conter 7 caracteres.");
+        }
+            
+    }
+                
+        
+        
+    }while 
+    
+}
