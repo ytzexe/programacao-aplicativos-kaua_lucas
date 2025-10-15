f9aa78ced1211827aa28e302a1bc67b1c560a3dd
Kauã Lucas <Kaualucass1122@gmail.com>
2025-10-14 21:32:51 -0300

    Update and rename Teste to ProjetoPratico.java

---
+++ Projeto Prático I/ProjetoPratico.java
@@ -0,0 +1,35 @@
+
+package projeto.pratico;
+
+import java.util.Scanner;
+
+public class ProjetoPratico {
+    
+
+   
+    public static void main(String[] args) {
+        
+         Scanner input = new Scanner(System.in);
+        
+       int escolha;
+       
+          do { 
+            System.out.print(
+                    "==== MENU INCIAL DO ESTACIONAMENTO ====\n"
+                            + " 1- REGISTRAR VEÍCULO EM VAGA\n"
+                            + " 2- FINALIZAR USO DO VEÍCULO NA VAGA\n"
+                            + " 3- CONSULTAR DISPONIBILIDADE DE VAGAS\n"
+                            + " 4- CONSULTAR SALDO E HISTÓRICO DE PAGAMENTOS\n"
+                            + " 0- SAIR\n"
+                            + " DIGITE A OPÇÃO DESEJADA: ");
+                                escolha = input.nextInt();
+                                input.nextInt();
+          switch(escolha){
+              case 1 ->
+          }
+        }
+    
+    
+
+}
+}
