d3f4d5f91f0ecde2240d1ad199331e4a5ee6faf9
Kauã Lucas <Kaualucass1122@gmail.com>
2025-10-16 12:19:53 -0300

    Add ConsultarVagas instance and update menu options

--- Projeto Prático I/ProjetoPratico.java
+++ Projeto Prático I/ProjetoPratico.java
@@ -7,10 +7,12 @@ public class ProjetoPratico {
     public static void main(String[] args) {
 
         RegistroVeiculo rv = new RegistroVeiculo();
+        ConsultarVagas cs = new ConsultarVagas();
         Scanner input = new Scanner(System.in);
 
         int escolha;
-
+        
+      
         do {
             System.out.print(
                     """
@@ -19,17 +21,17 @@ public class ProjetoPratico {
                      2 - FINALIZAR USO DO VEÍCULO NA VAGA
                      3 - CONSULTAR DISPONIBILIDADE DE VAGAS
                      4 - CONSULTAR SALDO E HISTÓRICO DE PAGAMENTOS
-                     0 - SAIR
-                     DIGITE A OPÇÃO DESEJADA:  """
-            );
-
+                     0 - SAIR""");
+                     System.out.print("\nDIGITE A OPÇÃO DESEJADA: ");
+                     
+            
             escolha = input.nextInt();
             input.nextLine();
 
             switch (escolha) {
                 case 1 -> rv.registrar();
                 case 2 -> System.out.println("Finalizar uso do veículo na vaga...");
-                case 3 -> System.out.println("Consultar disponibilidade de vagas...");
+                case 3 -> cs.consulta();
                 case 4 -> System.out.println("Consultar saldo e histórico de pagamentos...");
                 case 0 -> System.out.println("\nEncerrando o sistema...");
                 default -> System.out.println("\nOpção inválida! Tente novamente.\n");
