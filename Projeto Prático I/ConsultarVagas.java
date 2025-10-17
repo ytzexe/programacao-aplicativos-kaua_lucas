7da6dd91c0319860d68b76dc854c3a25d712ed22
Kauã Lucas <Kaualucass1122@gmail.com>
2025-10-16 21:35:25 -0300

    Add ConsultarVagas class to check parking availability

---
+++ Projeto Prático I/ConsultarVagas.java
@@ -0,0 +1,28 @@
+
+package projeto.pratico;
+
+
+class ConsultarVagas {
+  
+
+    void consulta() {
+  System.out.println("--- DISPONIBILIDADE DE VAGAS ---");
+        
+        int totalOcupadas = 0;
+        int totalLivres = 0;
+
+        for (int i = 0; i < RegistroVeiculo.MAX_VAGAS; i++) {
+            RegistroVeiculo veiculo = RegistroVeiculo.getVeiculoNaVaga(i);
+
+            if (veiculo != null) {
+                System.out.println("Vaga " + (i + 1) + " (Tipo: " + veiculo.getTipo() + "): OCUPADA - Placa: " + veiculo.getPlaca());
+                totalOcupadas++;
+            } else {
+              
+                System.out.println("Vaga " + (i + 1) + " (Tipo: 0): LIVRE");
+                totalLivres++;
+            }
+        }
+    }
+}
+
