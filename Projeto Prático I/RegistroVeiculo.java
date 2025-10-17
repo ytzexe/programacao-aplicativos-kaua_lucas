8a13fdd1ce2ef6c783ac3d4e8fc2d79d954e9a90
Kauã Lucas <Kaualucass1122@gmail.com>
2025-10-16 21:34:55 -0300

    Update RegistroVeiculo.java

--- Projeto Prático I/RegistroVeiculo.java
+++ Projeto Prático I/RegistroVeiculo.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class RegistroVeiculo {
 
-  
+    
     
        private int tipo;
        private String placa;
@@ -18,12 +18,30 @@ public class RegistroVeiculo {
        private String cor;
        private int hora;
        private int minuto;
-       private int vaga;
        
-    private static int codigo = 0;
-    private static int proximaVaga = 1;   
-    private static final int MAX_VAGAS = 10; 
+       private int vaga;
      
+    private static int codigo = 0;
+    private static int proximaVaga = 1;  
+    
+    public static final int MAX_VAGAS = 10;
+    private static RegistroVeiculo[] vagas = new RegistroVeiculo[MAX_VAGAS];
+     
+    public static RegistroVeiculo getVeiculoNaVaga(int indice) {
+        if (indice >= 0 && indice < MAX_VAGAS) {
+            return vagas[indice];
+        }
+        return null;
+    }
+   
+    public void setTipo(int tipo){
+        this.tipo = tipo;
+    }
+    
+    public void setTipo(){
+        System.out.println(tipo);
+    }
+    
     public void registrar(){
 
         Scanner input = new Scanner(System.in);
@@ -44,9 +62,10 @@ public class RegistroVeiculo {
                 + "2- Moto\n"
                 + "3- Caminhão/Caminhonete\n"
                 + "Digite o tipo do veículo: ");
-                tipo = input.nextInt();
-                input.nextLine();
-                if (tipo <1 || tipo >3){
+                
+                    tipo = input.nextInt();
+                    input.nextLine();
+                    if (tipo <1 || tipo >3){
                     System.out.println("Tipo invalido! Digite 1, 2 ou 3.");
                 }
              }while (tipo <1 || tipo >3);
@@ -60,19 +79,40 @@ public class RegistroVeiculo {
                     }
                 }while (placa.length() != 7);
                 
-            // INFORMAÇÕES NÃO USADA!    
-                System.out.print("Digite o nome do condutor: ");
-                nome = input.nextLine();
+            // INFORMAÇÕES NÃO USADA!  
+            
+                do {
+                    System.out.print("Digite o nome do condutor: ");
+                    nome = input.nextLine();
+                    if (nome.length() == 0){
+                        System.out.println("O nome do condutor não pode estar vazio!");
+                    }
+                }while(nome.length() == 0);
                 
-                System.out.print("Digite a marca do veículo: ");
-                marca = input.nextLine();
+                do{
+                    System.out.print("Digite a marca do veículo: ");
+                    marca = input.nextLine();
+                    if (marca.length() == 0){
+                        System.out.println("A marca não pode estar vazia!");
+                    }
+                }while(marca.length() == 0);
                 
-                System.out.print("Digite o modelo do veículo:");
-                modelo = input.nextLine();
+                do{
+                    System.out.print("Digite o modelo do veículo:");
+                    modelo = input.nextLine();
+                    if (modelo.length() == 0){
+                        System.out.println("O modelo não pode estar vazio!");
+                    }
+                }while(modelo.length() == 0);
                 
-                System.out.print("Digite a cor do veículo:");
-                cor = input.nextLine();
-                
+                do{
+                    System.out.print("Digite a cor do veículo:");
+                    cor = input.nextLine();
+                    if(cor.length() == 0){
+                        System.out.println("A cor não pode estar vazio");
+                    }
+                }while(cor.length() == 0);
+                                       
                 // HORA
                     do{
                         System.out.print("Digite a hora de entrada (0-23): ");
@@ -84,22 +124,32 @@ public class RegistroVeiculo {
                     
                     // MINUTO
                         do{
-                            System.out.println("Digite o minuto da entrada (0-59)");
+                            System.out.print("Digite o minuto da entrada (0-59): ");
                             minuto = input.nextInt();
                             if (minuto <0 || minuto >59){
                                 System.out.println("Minuto invalido! Digite apenas entre 0 as 59 minutos.");
                             }
+                           
                         }while (minuto <0 || minuto >59);
                         
                         // ATRIBUIÇÃO DE VAGA E CÓDIGO
                         
-                        vaga = proximaVaga;
-                        proximaVaga++;
-                        codigo++;
+//                        vaga = proximaVaga;
+//                        proximaVaga++;
+//                        codigo++;
                         
-                        System.out.println("Veículo registrado com suceso na vaga" + vaga + ".\nCódigo do v´eículo: " + codigo);
+                           RegistroVeiculo veiculo = new RegistroVeiculo();
+                            veiculo.tipo = this.tipo;
+                            veiculo.placa = this.placa;
+                            veiculo.hora = this.hora;
+                            veiculo.minuto = this.minuto;
+                            veiculo.vaga = codigo;
+                            vagas[codigo] = veiculo;
+                            
+                            System.out.println("Veículo registrado com sucesso na vaga " + (codigo + 1) + ".");
+                            codigo++;
                         
-                        
+                        // VERIFICAR SE DESEJA REGISTRAR OU VOLTAR AO MENU
                             System.out.print("Deseja registrar outro veículo? (S/N): ");
                             opcao = input.next().toUpperCase().charAt(0);
                             input.nextLine(); 
@@ -108,6 +158,7 @@ public class RegistroVeiculo {
          }while (opcao == 'S'); 
          
     }
+
          
          
           // GETTERS
